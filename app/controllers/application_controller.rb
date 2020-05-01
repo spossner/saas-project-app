9f153c8696ae06a667b61413d3ce55b75c11c36c
Sebastian Possner <sebastian.possner@brands4friends.de>
2020-05-01 11:27:09 +0200

    create Tenant with User.subdomain if a new user is created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  ##
+  # add subdomain to the trusted parameters
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :subdomain])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :subdomain])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  after_create :create_tenant
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def create_tenant
+    Apartment::Tenant.create(subdomain)
+  end
 end
